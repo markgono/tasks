2367dd74bd52a3d88c03ae13cc716868391c6ba2
Mark Gono <markgono@gmail.com>
2018-04-08 19:03:01 +0100

    add tasks routes, migration, views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * Returns all tasks
+ */
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->latest()->get();
+    
+    return view('tasks.index', compact('tasks'));
+});
+
+/**
+ * Returns our tasks as a nice JSON output. Perfect for APIs
+ */
+Route::get('/tasks/api', function () {
+  $tasks = DB::table('tasks')->get();
+
+  return $tasks;
+});
+
+/**
+ * Returns a task
+ */
+Route::get('tasks/{task}', function ($id) {
+  $task = DB::table('tasks')->find($id);
+  
+  return view('tasks.show', compact('task'));
 });
