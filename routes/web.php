bfda02dfc24c984130555de2195902ae1561c172
Mark Gono <markgono@gmail.com>
2018-04-19 21:35:57 +0100

    add photocontroller resource example

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,19 @@ Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/api', 'TasksController@api');
 
 Route::get('tasks/{task}', 'TasksController@show');
+
+/*
+  The following route is equivalent to:
+
+  Route::get('photos', 'PhotoController@index')
+  Route::get('photos/create', 'PhotoController@create')
+  Route::post('photos', 'PhotoController@store')
+  Route::get('photos/{photo}', 'PhotoController@show')
+  Route::get('photos/{photo}/edit', 'PhotoController@edit')
+  Route::put('photos/{photo}', 'PhotoController@update') *
+  Route::delete('photos/{photo}', 'PhotoController@destroy')
+
+  * can also be Route::patch
+*/
+Route::resource('photos', 'PhotoController');
+
