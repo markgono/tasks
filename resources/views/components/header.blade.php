190e063ae5da1a281b1757be4f0c838da0f45f59
Mark Gono <markgono@gmail.com>
2018-05-07 15:46:56 +0100

    add header authentication status

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,4 +2,10 @@
   <a href="\" class="navbar-brand">
       {!! file_get_contents(asset('svg/tasks-logo.svg')) !!}
   </a>
+
+  @if(Auth::check())
+    <a href="" class="nav-link ml-auto header__link">{{ Auth::user()['name'] }}</a>
+  @else
+    <a href="\login" class="nav-link ml-auto header__link">Login</a>
+  @endif
 </header>
