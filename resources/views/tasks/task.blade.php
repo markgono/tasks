2d4069dc333183179ae41cfaeb5ccb86e9ac05ac
Mark Gono <markgono@gmail.com>
2018-05-07 16:37:48 +0100

    show task author name and date in list-item

--- resources/views/tasks/task.blade.php
+++ resources/views/tasks/task.blade.php
@@ -2,7 +2,12 @@
   <a href="/tasks/{{ $task->id }}" class="task-item">
     <p class="task-item__number">{{ $task->id }}</p>
     <div class="task-item__content">
-      <h2 class="task-item__title">{{ $task->title }}</h1>
+      <h2 class="task-item__title">{{ $task->title }}</h2>
+      <small>
+        <span class="task-item__author">&ndash; {{ $task->user->name }}</span>
+        on 
+        <span class="task-item__date">{{ $task->created_at->toFormattedDateString() }}</span>
+      </small>
       <p class="task-item__body">{{ $task->body }}</p>
     </div>
   </a>
