cf821555a54e6eac62395dfb113d0e99c49ce495
Mark Gono <markgono@gmail.com>
2018-05-07 23:16:23 +0100

    add row, footer to default layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,11 +9,15 @@
 <body>
   @include('components.header')
   <main class="container mt-5" id="content" role="main">
-    @yield('content')
+    <div class="row">
+      @yield('content')
+    </div>
   </main>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/app.css">
-  @yield('footer')
+  <div class="mb-5">
+    @yield('footer')
+  </div>
 </body>
 </html>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layout')
 
 @section('content')
+<div class="col-sm-8">
   <h1>Tasks</h1>
   <ul>
     @foreach ($tasks as $task)
       @include('tasks.task')
     @endforeach
   </ul>
-  <div class="container mt-3">
+  <div class="mt-3">
     <a href="/tasks/create">Create a new task</a>
   </div>
+</div>
 @endsection
