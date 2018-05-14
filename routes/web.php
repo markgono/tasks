8ccc788d55deec11cde9ae703f39fec9caa5f2e4
Mark Gono <markgono@gmail.com>
2018-05-15 00:22:24 +0100

    add stripe service container example with simple binding

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,16 @@
-<?php 
+<?php
+
+use App\Billing\Stripe;
+
+/* We could make this a singleton with App::singleton
+*  No matter how many times we resolved, we'd get the same instance.
+*/
+App::bind('App\Billing\Stripe', function () {
+  return new Stripe(config('services.stripe.secret'));
+});
+
+// Could be: resolve('App\Billing\Stripe');
+$stripe = App::make('App\Billing\Stripe');
 
 Route::get('/', 'TasksController@index')->name('home');
 
