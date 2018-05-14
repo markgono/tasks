0ca544a7151c07ee8e4f2b34667ef7d154108dc4
Mark Gono <markgono@gmail.com>
2018-05-14 23:37:09 +0100

    Revert "add tasks repository automatic injection example"
    This reverts commit a9901f5491431c3a6aa830eb29e61845f96507f8.

--- app/Redis.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App;
-
-// Stub, for example
-class Redis
-{
-
-}
--- app/Repositories/Tasks.php
+++ app/Repositories/Tasks.php
@@ -3,15 +3,9 @@
 namespace App\Repositories;
 
 use App\Task;
-use App\Redis;
 
 class Tasks
 {
-  public function __construct(Redis $redis)
-  {
-    $this->redis = $redis;
-  }
-
   public function all()
   {
     return Task::all();
